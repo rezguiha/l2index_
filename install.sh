7a90967be3d86e872d073eec35f14fb399dbf9c1
Jean-Pierre Chevallet <jean-pierre.chevallet@imag.fr>
2021-05-03 18:45:43 +0200

    Complétion de l'install avec des bibliothèque installée par PIP

--- install.sh
+++ install.sh
@@ -49,15 +49,35 @@ fi
 #     $ conda deactivate
 
 
-# Installation de tensorflow l'in n'existe pas déjà
 echo "activate $MYENV"
 conda activate "$MYENV"
+
+# Installation de tensorflow s'il n'existe pas déjà
 if conda list | grep tensorflow
 then
   echo "Tensorflow already installed in env '$MYENV'"
 else
   conda install -c conda-forge tensorflow=1.15
 fi
+
+# Installation de fastttext s'il n'existe pas déjà => NE FONCTIONNE PAS AVEC CONDA
+#if conda list | grep fasttext
+#then
+#  echo "fasttext already installed in env '$MYENV'"
+#else
+#  conda install -c conda-forge fasttext
+#fi
+
+# Installe le reste avec PIP
+
+pip install fasttext
+pip install pytrec-eval
+pip install panda
+pip install pandas
+pip install nltk
+
+# Telecharge les stopwords
+python -c "import nltk;nltk.download('stopwords')"
 
 echo "END of script"
 echo "please activate env: conda activate $MYENV"
