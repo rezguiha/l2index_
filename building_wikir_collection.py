56fb29448eeddee7ca9c2512fa22e68bb5c48159
Hamdi Rezgui <rezguiha@ceos.imag.fr>
2021-05-13 13:38:55 +0200

    modifying the file to support inverted_structure and Queries instances

--- building_wikir_collection.py
+++ building_wikir_collection.py
@@ -1,5 +1,6 @@
 import argparse
-import wikIR_Collection
+import wikIR_Collection_opt
+import time
 
 
 ################2. Building Wikir Collections####################
@@ -13,22 +14,19 @@ def main():
     args = parser.parse_args()
 
     print(args, flush=True)
+    print("------------------start---------------",flush=True)
+    print('--------Reading documents', flush=True)
+    start=time.time()
+    Collection_wikir = wikIR_Collection_opt.Collection('english')
+    Collection_wikir.load_documents(args.coll_path)
+    
+    print('------Creating Posting file, Vocabulary and Document length ', flush=True)
+    Collection_wikir.build_inverted_index_and_vocabulary(args.indexed_path,save=True)
 
-    print('Reading collection', flush=True)
-
-    Collection_wikir = wikIR_Collection.Collection('english')
-    Collection_wikir.load_collection(args.coll_path)
-
-    print('Standard Preprocess', flush=True)
-    Collection_wikir.standard_preprocess(remove_stopwords=True,
-                                         min_occ=5)
-
-    print('Compute inverted index', flush=True)
-    Collection_wikir.compute_info_retrieval()
-    print('Compute fasstetext embeddings', flush=True)
-    Collection_wikir.compute_fasttext_embedding(args.fasttext_path)
-    print('Saving pickle file',flush=True)
-    Collection_wikir.pickle_indexed_collection(args.indexed_path + '/indexed_collection')
-    print('Finished',flush=True)
+#     print('Compute fasstetext embeddings', flush=True)
+#     Collection_wikir.compute_fasttext_embedding(args.fasttext_path)
+    end=time.time()
+    print('--------------------Finished------------------',flush=True)
+    print("Total time = ", end-start, " s",flush=True)
 if __name__ == "__main__":
     main()
