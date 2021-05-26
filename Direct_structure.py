2f7a7cef32088b07c5764d78eefe7e4896ce04e6
Jean-Pierre Chevallet <jean-pierre.chevallet@imag.fr>
2021-05-26 11:54:14 +0200

    Add filter vocabulary to Direct structure to compute a new collection with reduced vocabulary

--- Direct_structure.py
+++ Direct_structure.py
@@ -30,6 +30,50 @@ class Direct_structure:
         with open(file_path+'/processed_documents','wb') as f:
             processed_document.tofile(f)
 
+    def filter_vocabulary(self,new_vocabulary):
+        """
+        Change the vocabulary of all documents
+        new_vocabulary: an array of unsing int of new token id indexed by the previous id
+        At the end of this process, the whole collection is represented in the new token id
+        To filter the id, some old token id are associated to the max unsigned value: 0xffffffff
+        As a consequence of this transformation and filtering, documents may be shorter
+        """
+        # Position of the old term in current document
+        oldPos = 0
+        # Position of the new term in current document
+        newPos = 0
+        # The new size of all document content
+        newContentSize = 0;
+        # Filter all documents
+        for docId in range(len(self.doc_length)):
+            # New current document size
+            newDocSize = 0
+            # for all token of this document
+            for tokenNb in range(self.doc_length[docId])
+                # Get current tocken id
+                oldId = self.processed_documents[oldPos]
+                # Get new tocken
+                newId = new_vocabulary[oldId]
+                # Test if this tocken is removed
+                if newId == 0xffffffff:
+                    # this tocken is removed, so we only move to next old tocken
+                    oldPos += 1
+                else:
+                    # tocken not removed, so added to current document
+                    self.processed_documents[newPos] = newId
+                    # next token and next position in the document
+                    oldPos += 1
+                    newPos += 1
+                    newDocSize += 1
+                    newContentSize += 1
+            # Store the new document size
+            self.doc_length[docId] = newDocSize
+        # All document are filtered, remove the extra space if necessary
+        if newContentSize < len(self.processed_documents):
+            # We hope that python is smart enought for managing memory here
+            self.processed_documents = self.processed_documents[:newContentSize]
+
+
     def saving_all_documents(self,file_path):
         """
         Saving all documents that has been processed and added to the overall array
@@ -43,7 +87,7 @@ class Direct_structure:
         with open(file_path+'/documents_length', 'rb') as f:
             #We use frombytes because it does not require to enter the number of elements you want to retrieve. fromfile does.
             self.doc_length.frombytes(f.read())
-        
+
         #Loading processed documents
         with open(file_path+'/processed_documents', 'rb') as f:
             for document_length in self.doc_length:
@@ -52,6 +96,3 @@ class Direct_structure:
                 self.processed_documents.append(processed_document)
     def get_number_of_document(self):
         return len(self.doc_length)
-    
-    
- 
